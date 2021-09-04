779bd404e46992c5e7bbbe0feafe626a677273a6
geriavrazi00 <45440480+geriavrazi00@users.noreply.github.com>
2021-09-04 22:05:48 +0200

    Started the project with basic tests

---
+++ src/App.vue
@@ -0,0 +1,49 @@
+<template>
+  <!-- <img alt="Vue logo" src="./assets/logo.png">
+  <HelloWorld msg="Welcome to Your Vue.js App"/> -->
+  <h1>{{ title }}</h1>
+  <input type="text" ref="name">
+  <button @click="handleClick">click me</button>
+  <Modal :header="header" :text="text" :arr="arr" theme="sale"/>
+</template>
+
+<script>
+import Modal from './components/Modal.vue'
+
+export default {
+  name: 'App',
+  components: { Modal },
+  data() {
+    return {
+      title: 'My First Vue Project',
+      arr: ["test", 265],
+      header: 'Sign up for the Giveaway!',
+      text: 'Grab them by the pussy!'
+    }
+  },
+  methods: {
+    handleClick() {
+      // $refs - referenca per elemente te ndryshme te domit
+      console.log(this.$refs.name);
+      this.$refs.name.classList.add('active');
+    }
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+
+h1 {
+  border-bottom: 1px solid #ddd;
+  display: inline-block;
+  padding-bottom: 10px;
+}
+</style>
