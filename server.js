5fd7d957e6a1bdc95dc214be48f19a87db78eb09
geriavrazi00 <45440480+geriavrazi00@users.noreply.github.com>
2021-09-17 02:37:49 +0200

    Work on the help component

--- server.js
+++ server.js
@@ -1,31 +1,44 @@
-// const express = require('express');
-// const serveStatic = require("serve-static")
-// const path = require('path');
-// app = express();
-// app.use(serveStatic(path.join(__dirname, 'dist')));
-// const port = process.env.PORT || 3000;
-// app.listen(port);
-
 const express = require('express');
+const serveStatic = require("serve-static")
 const path = require('path');
-const history = require('connect-history-api-fallback');
+app = express();
+app.use(serveStatic(path.join(__dirname, 'dist')));
+const port = process.env.PORT || 3000;
+app.listen(port);
 
-const app = express();
+// const express = require('express');
+// const path = require('path');
+// const history = require('connect-history-api-fallback');
 
-const staticFileMiddleware = express.static(path.join(__dirname + '/dist'));
+// const app = express();
 
-app.use(staticFileMiddleware);
-app.use(history({
-  disableDotRule: true,
-  verbose: true
-}));
-app.use(staticFileMiddleware);
+// const staticFileMiddleware = express.static(path.join(__dirname + '/dist'));
 
-app.get('/', function (req, res) {
-  res.render(path.join(__dirname + '/dist/index.html'));
-});
+// app.use(staticFileMiddleware);
+// app.use(history({
+//   disableDotRule: true,
+//   verbose: true
+// }));
+// app.use(staticFileMiddleware);
 
-var server = app.listen(process.env.PORT || 8080, function () {
-  var port = server.address().port;
-  console.log("App now running on port", port);
-});
+// app.get('/', function (req, res) {
+//   res.render(path.join(__dirname + '/dist/index.html'));
+// });
+
+// var server = app.listen(process.env.PORT || 8080, function () {
+//   var port = server.address().port;
+//   console.log("App now running on port", port);
+// });
+
+// var history = require('connect-history-api-fallback');
+
+// var express = require('express')
+// var path = require('path')
+// var serveStatic = require('serve-static')
+// app = express()
+// //add this middleware
+// app.use(history());    
+// app.use(serveStatic(__dirname))
+// var port = process.env.PORT || 5000
+// app.listen(port)
+// console.log('server started '+ port) 
